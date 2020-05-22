7fc5389979664e521e672aa0e0701ffa0aeb7f78
Luis Paez <luis2832.lefp@gmail.com>
2020-05-22 12:58:49 -0600

    Esta si podria ser la version 0.1

---
+++ js/acciones.js
@@ -0,0 +1,5 @@
+//Esta funcion abre la pagina para ver universidades con el click en el boton
+function abrirU(){
+    //Este metodo abre la pagina
+    window.open("universidades.html","Universidades",);
+}
